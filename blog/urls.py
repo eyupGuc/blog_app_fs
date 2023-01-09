140a1b006941b20df3403991bfd5445f6fd23372
eyupGuc <meydanguresi@gmail.com>
2023-01-09 22:11:29 +0300

    api

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,14 @@
 
-from django.urls import path
+from django.urls import path, include
+from .views import CategoryView
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register('category',CategoryView)
+
 
 urlpatterns = [
 
-    # path('category/',))
+    path('', include(router.urls))
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Category
+from .serializers import CategorySerializer
 
 
 class CategoryView(ModelViewSet):
     queryset = Category.objects.all()
-    serializer_class = None
+    serializer_class = CategorySerializer
     
 
