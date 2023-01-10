c08415e0a335b51621b49f6b897da507799a4310
eyupGuc <meydanguresi@gmail.com>
2023-01-10 12:11:01 +0300

    end

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+
 from .models import Category, Blog
 from .serializers import CategorySerializer, BlogSerializer
+from .permissions import IsAdminOrReadOnly
 
 
 class CategoryView(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrReadOnly]
     filterset_fields = ['name']
  
     
@@ -14,6 +19,7 @@ class CategoryView(ModelViewSet):
 class BlogView(ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
     filterset_fields = ['category']
     search_fields = ['title', 'content']
     
